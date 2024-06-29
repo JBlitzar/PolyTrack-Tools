af9a5bd42abae46bf4c9f98f8a3f2feb9ca304d5
JBlitzar <90664831+JBlitzar@users.noreply.github.com>
2024-06-28 18:05:48 -0700

    Update main.py

--- main.py
+++ main.py
@@ -3,21 +3,33 @@ from shapebuilder import ShapeBuilder
 from titlebuilder import TitleBuilder
 from trackbuilder import TrackBuilder
 from voxelbuilder import VoxelBuilder
-
+import time
 
 from chainer import Chainer
 
 
+for i in range(100):
+     chainer = Chainer("skibidi")
 
-chainer = Chainer("Skibidi toilet")
+
+
+     chainer.chain(TrackBuilder(), "add_starting_point", {"pos":(0, 1, 0)})
+
+     pieces = []
 
 
 
-chainer.chain(TrackBuilder(), "add_starting_point", {"pos":(0, 31, 50)})
+     for j in range(i):
+          pieces.append({"id":52,"x":1,"y":1,"z":j+1})
+     
+     pieces.append({"id":6,"x":1,"y":1,"z":i+2})
+     print(pieces)
+     time.sleep(1)
+     chainer.chain(TrackBuilder(), "add_pieces", {"pieces":pieces})
 
-chainer.chain(TitleBuilder(), "create_cover_image", {"title":"JBlitzar", "subtitle":"", "offset":(100,0,0), "negate":True})
+     #chainer.chain(TitleBuilder(), "create_cover_image", {"title":"JBlitzar", "subtitle":"", "offset":(100,0,0), "negate":True})
 
-chainer.chain(VoxelBuilder(), "add_file", {"file_path":"toilet.obj", "scale_factor":5})
+     #chainer.chain(VoxelBuilder(), "add_file", {"file_path":"toilet.obj", "scale_factor":5})
 
-with open("out.txt", "w+") as file: 
-     file.write(chainer.export())
+     with open("out.txt", "a") as file:
+          file.write(f"\nStraightaway: {i+1} "+chainer.export())
